2b8a1d86b7fa90209bde50d64dc682cef7fc591c
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-03-21 22:32:29 +0800

    Add files via upload

---
+++ test_3.21/test_4.26/test_4.26.c
@@ -0,0 +1,138 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int a = 4;
+//	int b = 5;
+//	if (a >= b)
+//	{
+//		printf("hehe\n");
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int b = 0;
+//	/*if (a && b)
+//	{
+//		printf("hehe\n");
+//	}*/
+//	/*if (a || b)
+//	{
+//		printf("haha\n");
+//	}*/
+// 	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int m = 0;
+//	//(a>b) ? (m = a) : (m = b);
+//	m = (a > b ? a : b);
+//
+//	/*if (a > b)
+//		m = a;
+//	else
+//		m = b;*/
+//	printf("%d\n", m);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 2;
+//	int c = 5;
+//	int d = (a+=3, b=5, c=a+b, c-4);
+//	//      a=6    b=5  c=11   7
+//	printf("%d\n", d);
+//	return 0;
+//}
+//
+
+//int main()
+//{
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	//             0 1 2 3 4 5 6 7 8 9 
+//	printf("%d\n", arr[5]);
+//	//
+//	//3+5
+//	//
+//	return 0;
+//}
+//
+//
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//int main()
+//{
+//	int z = Add(3, 5);
+//	printf("hehe");
+//	return 0;
+//}
+//
+
+//int main()
+//{
+//	int num = 0;
+//
+//	return 0;
+//}
+
+//typedef unsigned int u_int;
+//typedef unsigned long long ull;
+//
+//int main()
+//{
+//	unsigned int num = 0;
+//	u_int num2 = 0;
+//	ull num3 = 0;
+//
+//	return 0;
+//}
+//
+
+//typedef struct Node
+//{
+//	int data;
+//	struct Node* next;
+//}Node;
+
+
+//
+//static 修饰局部变量的时候
+
+
+//void test()
+//{
+//	static int a = 5;
+//	a++;
+//	printf("%d ", a);
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//	}
+//
+//	return 0;
+//}
