9674cbbfbbfb7c551312fa1d020ac090d009a392
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-04-03 12:14:39 +0800

    Add files via upload

---
+++ test_4.3/Project5/test_5.15.c
@@ -0,0 +1,110 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int a = 100;
+//	int * pa = &a;
+//	char* pc = &a;
+//
+
+//	//int arr[10];
+//	//printf("%p\n", &a);
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(char*));
+//	printf("%d\n", sizeof(short*));
+//	printf("%d\n", sizeof(int*));
+//	printf("%d\n", sizeof(long*));
+//	printf("%d\n", sizeof(float *));
+//	printf("%d\n", sizeof(double*));
+//
+//	return 0;
+//}
+//
+
+
+
+//int main()
+//{
+//	int a = 0x11223344;
+//	char* pa = &a;
+//	*pa = 0;
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int a = 0x11223344;
+//
+//	int* pa = &a;
+//	char* pc = &a;
+//	printf("%p\n", pa);
+//	printf("%p\n", pc);
+//
+//	printf("%p\n", pa+1);
+//	printf("%p\n", pc+1);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	*pa = 20;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//“∞÷∏’Î
+//int main()
+//{
+//	int* p;
+//	*p = 20;
+//	printf("%d\n", *p);
+//
+//	return 0;
+//}
+//
+
+
+//int* test()
+//{
+//	int a = 110;
+//	return &a;
+//}
+//
+//int main()
+//{
+//	int* p = test();
+//	printf("%d\n", *p);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;
+//
+//	int* ptr = NULL;
+//	//int* ptr2;
+//
+//	if (ptr != NULL)
+//	{
+//		// π”√
+//	}
+//
+//	return 0;
+//}
+//
