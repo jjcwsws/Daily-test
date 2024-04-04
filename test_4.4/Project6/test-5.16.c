52ceca72f7701a13a991ec2bc821c2ecc7252f59
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-04-04 21:34:31 +0800

    Add files via upload

---
+++ test_4.4/Project6/test-5.16.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int main()
+//{
+//    int arr[] = { 1,2,3,4,5 };
+//    short* p = (short*)arr;
+//    int i = 0;
+//    for (i = 0; i < 4; i++)
+//    {
+//        *(p + i) = 0;
+//    }
+//
+//    for (i = 0; i < 5; i++)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a = 0x11223344;
+//    char* pc = (char*)&a;
+//    *pc = 0;
+//    printf("%x\n", a);
+//    return 0;
+//}
+
+unsigned long pulArray[] = { 6,7,8,9,10 };
+unsigned long* pulPtr;
+pulPtr = pulArray;
+*(pulPtr + 3) += 3;
+printf("%d,%d\n", *pulPtr, *(pulPtr + 3));
