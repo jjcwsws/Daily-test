cd20d6a07520ca5f7149c8c6dd0c19e04d8682f9
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-04-18 21:48:37 +0800

    Add files via upload

---
+++ test_4.18/work 4_12/work 4_12/assignment_4_12.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//
+//int sum(int a)
+//{
+//    int c = 0;
+//    static int b = 3;
+//    c += 1;
+//    b += 2;
+//    return (a + b + c);
+//}
+//int main()
+//{
+//    int i;
+//    int a = 2;
+//    for (i = 0; i < 5; i++)
+//    {
+//        printf("%d,", sum(a));
+//    }
+//}
+//
+//int main()
+//{
+//    int h = 0;
+//    int m = 0;
+//    int k = 0;
+//    scanf("%d:%d %d", &h, &m, &k);
+//
+//    h = (h + (k + m) / 60) % 24;
+//    m = (k + m) % 60;
+//
+//    printf("%02d:%02d\n", h, m);
+//
+//    return 0;
+//}
+//
+//int main()
+//{
+//    char i = 0;
+//    scanf("%c", &i);
+//    printf("%d\n", i);
+//    return 0;
+//}
