69485cc6d6b94f9f9c65dcacbd9b8c98030b681e
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-05-12 22:16:05 +0800

    Add files via upload

---
+++ 5.12/5.12.py
@@ -0,0 +1,17 @@
+year = int(input("请输入一个年份"))
+if(year%4==0 and (year%i0o)!=0)or (year%4==0):
+    month = int(input("请输入一个月份"))
+    if(month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
+        print("此月有31天")
+    elif(month == 7):
+        print("此月有28天")
+    else:
+        print("此月有30天")
+else: 
+        month = int (input("请输入一个月份"))
+        if(month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
+            print("此月有31天")
+        elif(month == 7):
+            print("此月有28天")
+        else:
+            print("此月有30天")
---
+++ 5.12/5.9.py
@@ -0,0 +1,32 @@
+########s = [1,2,3,4,5,6,7,8,9,0]
+########for i in range(10):
+########    if s[i] % 2 == 0:
+########        s[i] = s[i]**2
+########print(s)
+######
+######
+######
+######score1 = (78,92,74)
+######score2 = (87,85)
+######all_scores = score1 + score2
+######print(all_scores)
+######
+######
+####
+####
+####import random
+####random_set=set()
+####while len(random_set)<50:
+####    random_set.add(random.randint(0,100))
+####print(random_set)
+##
+##
+##import random
+##t = [random.randint(0,100)for i in range(20)]
+##d1 = t[0:10]
+##d1.sort()
+##t[0:10] = d1
+##d1 = t[10:20]
+##d1.sort(reverse=True)
+##t[10:20] = d1
+##print(tuple(t))
