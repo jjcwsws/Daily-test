4a427a549ee90ac99a082f014b2f1c5a29516c09
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-05-13 23:49:43 +0800

    Add files via upload

---
+++ 5.13/1.py
@@ -0,0 +1,28 @@
+##import math
+##class  长方形:
+##    def __init__(self,a,b):
+##        self.a = a
+##        self.b = b
+##    def get周长(self):
+##        return 2*(self.a + self.b)
+##    def get面积(self):
+##        return self.a*self.b
+##r1 = 长方形(4,5)
+##r2 = 长方形(3,6)
+##
+##print("长方形 1：")
+##print("周长:",r1.get周长())
+##print("面积:",r1.get面积())
+##
+##print("长方形 2：")
+##print("周长:",r2.get周长())
+##print("面积:",r2.get面积())
+
+
+##def _max3(num1,num2,num3):
+##    return max(num1,num2,num3)
+##print(max 3(5,10,15))
+##print(max 3(20,3,7))
+##print(max 3(12,12,12))
+
+
