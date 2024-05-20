4a440f4c5e2d1d8fee7f171381ed47068904e72f
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-05-20 23:47:59 +0800

    Add files via upload

---
+++ test_5_20/test_5_20/test.c
@@ -0,0 +1,102 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//void test()
+//{
+//	printf("hehe\n");
+//	printf("test\n");
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//
+//	int i = 0;
+//	test();
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = 10 - i;
+//	}
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+
+//void test2()
+//{
+//	printf("test2\n");
+//}
+//void test1()
+//{
+//	test2();
+//}
+//void test()
+//{
+//	test1();
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+//
+//
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;//保存最终结果
+//	int n = 0;
+//	int ret = 1;//保存n的阶乘
+//
+//	scanf("%d", &n);//3
+//
+//	for (i = 1; i <= n; i++)//1 2 3
+//	{
+//		int j = 0;
+//		ret = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//
+//1!+2!+3! = 1+2+6 = 9
+//15
+//
+
+
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	for (i = 0; i <= 12; i++)
+	{
+		arr[i] = 0;
+		printf("hehe\n");
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
