9e01871c8c06043ad562f6657d8a4679879372c7
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-06-22 22:25:48 +0800

    Add files via upload

---
+++ test_6.22/6.22/Stack.cpp
@@ -0,0 +1,16 @@
+#include"Stack.h"
+
+namespace bit
+{
+	//void StackInit(ST* ps)
+	//{
+	//	ps->a = NULL;
+	//	ps->top = 0;
+	//	ps->capacity = 0;
+	//}
+
+	//void StackPush(ST* ps, int x)
+	//{
+	//	//...
+	//}
+}
