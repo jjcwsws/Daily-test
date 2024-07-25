e4649e48e5e626f8fc40079b7881b0b29fcf9427
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-07-26 00:02:59 +0800

    Add files via upload

---
+++ test_7.26/AVLTree.h
@@ -0,0 +1,101 @@
+#pragma once
+
+template<class K, class V>
+struct AVLTreeNode
+{
+	AVLTreeNode<K, V>* _left;
+	AVLTreeNode<K, V>* _right;
+	AVLTreeNode<K, V>* _parent;
+	int _bf; // balance factor
+	pair<K, V> _kv;
+
+	AVLTreeNode(const pair<K, V>& kv)
+		:_left(nullptr)
+		, _right(nullptr)
+		, _parent(nullptr)
+		, _bf(0)
+		, _kv(kv)
+	{}
+};
+
+template<class K, class V>
+class AVLTree
+{
+	typedef AVLTreeNode<K, V> Node;
+public:
+	bool Insert(const pair<K, V>& kv)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(kv);
+			return true;
+		}
+
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->kv.first < kv.first)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		cur = new Node(kv);
+		if (parent->_kv.first < kv.first)
+		{
+			parent->_right = cur;
+		}
+		else
+		{
+			parent->_left = cur;
+		}
+		cur->_parent = parent;
+
+		while (parent)
+		{
+			if(cur == parent->left)
+			{
+				parent->_bf--;
+			}
+			else
+			{
+				parent->_bf++;
+			}
+
+			if (parent->_bf == 0)
+			{
+				break;
+			}
+			else if (parent->_bf == 1 || parent->_bf == -1)
+			{
+				cur = cur->_parent;
+				parent = parent->_parent;
+			}
+			else if (parent->_bf == 2 || parent->_bf == -2)
+			{
+				// 旋转处理
+			}
+			else
+			{
+				// 插入之前AVL树就有问题
+				assert(false);
+			}
+		}
+
+		return true;
+	}
+
+private:
+	Node* _root = nullptr;
+};
