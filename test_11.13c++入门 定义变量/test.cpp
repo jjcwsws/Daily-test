926a7d360644705a297b141fd75358309076e5c2
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-11-13 20:04:19 +0800

    Add files via upload

---
+++ test_11.13c++入门 定义变量/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char a;
+    int b;
+    long c;
+    double d;
+    cout << sizeof(a) << endl;
+    cout << sizeof(b) << endl;
+    cout << sizeof(c) << endl;
+    cout << sizeof(d) << endl;
+
+
+    return 0;
+}
