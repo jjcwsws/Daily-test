b5cdbcfbab7be328fd6c03e6ced9dc825719b839
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-11-26 23:31:19 +0800

    Add files via upload

--- test_11.26/test.c
+++ test_11.26/test.c
@@ -1,93 +1,115 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #pragma warning(disable:6031)
 #include <stdio.h>
-//找整除数,法1分支语句
-int main()
+//void reverse_string(char* arr)
+//{
+//	int len = strlen(arr);
+//	char p = *arr;
+//	*arr = *(arr + len - 1);
+//
+//	*(arr + len - 1) = '\0';
+//	if (strlen(arr + 1) >= 2)
+//		reverse_string(arr + 1);
+//
+//	*(arr + len - 1) = p;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	
+//	reverse_string(arr);
+//	return 0;
+//}
+//求字符串长度，递归
+//int strlen_string(char* arr)
+//{
+//	if (*arr == '\0')
+//		return 0;//遇到字符0，返回0，递归结束
+//	else
+//		return 1 + strlen_string(arr + 1);//递归调用
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int a=strlen_string(arr);
+//	printf("%d", a);
+//	return 0;
+//}
+//非递归
+//int strlen_string(char* arr)
+//{
+//	int count = 0;//判断字符变量
+//	while (1)
+//	{
+//		if (*arr != '\0')
+//		{		
+//			count++;
+//			arr++;
+//		}
+//		else
+//			break;
+//	}
+//	return count;
+//	
+//}
+// 求n的阶乘
+// int fac(int n)
+//{
+//	if (n == 0)
+//		return 1;
+//	else
+//		return n * fac(n - 1);
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int b = fac(a);
+//	printf("%d", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[] = "abcdefg";
+//	int a = strlen_string(arr);
+//	printf("%d", a);
+//	return 0;
+//}
+// 非递归
+//int fac(int n)
+//{
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret * i;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int b = fac(a);
+//	printf("%d", b);
+//	return 0;
+//}
+//打印整数的每一位
+void fun(int n)
 {
-    int a = 0;
-    scanf("%d", &a);
-    if (a % 2 == 0)
-    {
-        if (a % 3 == 0 && a % 7 == 0)
-        {
-            printf("2 3 7");
-        }
-        else if (a % 3 == 0)
-        {
-            printf("2 3");
-        }
-        else if (a % 7 == 0)
-        {
-            printf("2 7");
-        }
-        else
-        {
-            printf("2");
-        }
-    }
-    else if (a % 3 == 0)
-    {
-        if (a % 2 == 0 && a % 7 == 0)
-        {
-            printf("2 3 7");
-        }
-        else if (a % 2 == 0)
-        {
-            printf("2 3");
-        }
-        else if (a % 7 == 0)
-        {
-            printf("3 7");
-        }
-        else
-        {
-            printf("3");
-        }
-    }
-    else if (a % 7 == 0)
-    {
-        if (a % 2 == 0 && a % 3 == 0)
-        {
-            printf("2 3 7");
-        }
-        else if (a % 2 == 0)
-        {
-            printf("2 7");
-        }
-        else if (a % 3 == 0)
-        {
-            printf("3 7");
-        }
-        else
-        {
-            printf("7");
-        }
-    }
-    else
-    {
-        printf("n");
-    }
-    return 0;
+	if (n / 10 == 0)
+		printf("%d ", n);
+	else
+	{
+		fun(n / 10);
+		printf("%d ", n%10);
+	}
 }
-//法2，数组，循环
 int main()
 {
-    int a;//输入变量
-    int i = 0;//循环控制变量
-    int b = 0;//计数器
-    int arr[3] = { 2,3,7 };
-    scanf("%d", &a);
-    for (i = 0; i < 3; i++)
-    {
-        if (a % arr[i] == 0)
-        {
-            printf("%d ", arr[i]);
-            b++;
-        }
-    }
-    if (b == 0)
-    {
-        printf("n");
-    }
-    return 0;
+	int a = 0;
+	scanf("%d", &a);
+    fun(a);
+	return 0;
 }
