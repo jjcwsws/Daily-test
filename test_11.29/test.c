e448e400a19e3eb51b489738a87935a1968889d5
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-11-29 23:16:03 +0800

    Add files via upload

---
+++ test_11.29/test.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+#include <stdio.h>
+#include <string.h>
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = Add(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//    int count = 0;
+//    int i = 0;
+//    for (i = 1; i <= 2019; i++)
+//    {
+//        int t = i;
+//        while (t)
+//        {
+//            if (t % 10 == 9)
+//            {//余数为9，说明包含9
+//                count++;//结果数就加一
+//                break;
+//            }
+//            t /= 10;
+//        }
+//    }
+//    printf("%d", count);
+//    return 0;
+//}
+#include <stdio.h>
+
+//int main()
+//{
+//    int m = 0;
+//    int a, b, c;
+//    int count = 0;
+//    scanf("%d", &m);
+//    while (m)
+//    {
+//        scanf("%d%d%d", &a, &b, &c);
+//        int ret = (a + b + c) / 3;
+//        if (ret < 60)
+//            count++;
+//        getchar();
+//        m--;
+//    }
+//    printf("%d", count);
+//    return 0;
+//}
