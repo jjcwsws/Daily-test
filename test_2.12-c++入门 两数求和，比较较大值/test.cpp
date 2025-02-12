da9f0361840d1cf14db3a7c3a263cc167e141dd3
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2025-02-12 13:41:42 +0800

    Add files via upload

---
+++ test_2.12-c++入门 两数求和，比较较大值/test.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b;
+    cin >> a >> b;
+    cout << (a > b ? a : b) << endl;
+
+
+   /* int a;
+    int b;
+    cin >> a >> b;
+    cout << a + b << endl;*/
+
+    return 0;
+}
+
