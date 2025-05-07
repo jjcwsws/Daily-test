2159c37b3f9b6cb331bd025d3375aaa6f8c07044
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2025-05-07 22:53:03 +0800

    Add files via upload

---
+++ test_5.7/test.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string convertDateToBinary(string date) {
+        return format("{:b}-{:b}-{:b}",
+            stoi(date.substr(0, 4)),
+            stoi(date.substr(5, 2)),
+            stoi(date.substr(8, 2)));
+    }
+};
+
